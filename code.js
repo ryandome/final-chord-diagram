4ec6884d2554c620706defd4e8a1fd1fffbd722f
Ryan Dome <ryan.dome@centre.edu>
2019-12-03 11:11:17 -0500

    labels

--- code.js
+++ code.js
@@ -128,6 +128,9 @@ svg.datum(exp)
     .on("mouseover", showTooltip)
     .on("mouseout", hideTooltip)
 
+
+
+
 /*
 var innerRadius = 355
 
@@ -144,6 +147,119 @@ svg.append("svg:text")
         })
     .text(function(d,i) {return names[i]; });
 */
+
+
+
+
+//M2.2349804084439196e-14,-365A365,365,0,0,1,265.03252384807706,-250.96366530380138L257.7713588111435,-244.08794844616298A355,355,0,0,0,2.173748068486552e-14,-355Z
+
+
+
+
+var svgPath = d3.select("#pathChina")
+                .append("svg")
+                .attr("width", 800)
+                .attr("height", 800)
+                .attr("id", "Pathsvg");
+/*
+svgPath.append("path")
+        .attr("id", "chinaPath")
+        .attr("d", "M2.2349804084439196e-14,-365A365,365,0,0,1,265.03252384807706,-250.96366530380138L257.7713588111435,-244.08794844616298A355,355,0,0,0,2.173748068486552e-14,-355Z" )
+        .style("fill", "none")
+        .style("stroke", "#AAAAAA");
+
+svgPath.append("text")
+        .append("textPath")
+        .attr("xlink:href", "#chinaPath")
+        .style("text-anchor", "middle")
+        .attr("startOffset", "50%")
+        .text("China")
+        
+*/
+
+
+
+
+var pieGenerator = d3.pie()
+  .value(function(d) {return d.quantity;})
+  .sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+//var data = [ 34575, 22830, 20953, 15824, 8357, 15991, 9523, 15824, 6359, 12135, 162371 ];
+
+
+
+
+
+var Countries = [
+    {name: 'China', quantity: 34575},
+    {name: 'Canada', quantity: 22830},
+    {name: 'Mexico', quantity: 20953},
+    {name: 'Japan', quantity: 15824},
+    {name: 'Germany', quantity: 8357},
+    {name: 'United Kingdom', quantity: 16732},
+    {name: 'South Korea', quantity: 9523},
+    {name: 'India', quantity: 15824},
+    {name: 'France', quantity: 7895},
+    {name: 'Brazil', quantity: 12135},
+    {name: 'United States', quantity: 79274},   
+];
+
+var arcData = pieGenerator(Countries);
+
+
+var arcGenerator = d3.arc()
+  .innerRadius(20)
+  .outerRadius(365);
+
+d3.select('#Pathsvg')
+  .selectAll('path')
+  .data(arcData)
+  .enter()
+  .append('path')
+  .style("stroke", "white")
+  .attr("transform", "translate(375,375)")
+  .attr('d', arcGenerator);
+
+d3.select("#Pathsvg")
+    .selectAll("text")
+    .data(arcData)
+    .enter()
+    .append("text")
+    .each(function(d)
+        {
+        var centroid = arcGenerator.centroid(d);
+		d3.select(this)
+			.attr('x', centroid[0])
+			.attr('y', centroid[1])
+			.attr('dy', '0.33em')
+            //.attr("dx", "21em")
+			.text(d.data.name)
+            .style("fill", "white")
+            .style("text-anchor", "middle")
+    .attr("transform", "translate(375,375)")
+            
+        });
+    /*
+    .attr("transform", function(d) 
+        {
+            return "translate(" + arc.centroid(d) + ")"
+        })
+    .attr("text-anchor","middle")
+    .text( function(d){ return d.Countries.name})
+    */
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
